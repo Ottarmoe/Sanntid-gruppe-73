40f0c767491efb6fba450e57f34552ec8b0cc288
Anders Skaret Johansen <anders.skaret.johansen@gmail.com>
2026-02-09 18:55:43 +0800

    diverse

---
+++ elevator/state/state.go
@@ -0,0 +1,52 @@
+package state 
+
+type hallOrderState int
+
+const (
+	HallO hallOrderState = iota
+	HallNO
+	HallOPR
+)
+
+type cabOrderState int
+
+const (
+	CabO cabOrderState = iota
+	CabNO
+	CabUO //unknown order
+)
+
+type State struct {
+    HallOrders    	[4][2]hallOrderState 
+    CabOrders     	[4]cabOrderState      
+    CabFloor   	 	string    
+    CabDir      
+	CabBehavoiur
+	CabMechError
+	OnNetwork		bool
+}
+
+func finiteStateMachine(
+	buttonClick <-chan ButtonEvent, 
+	floorReached <-chan int, 
+	stopClick <-chan bool, 
+	obstructionChange <-chan bool) {
+	
+	for {
+		select {
+		case a := <- buttonClick:
+			fmt.Printf("%+v\n", a)
+			
+		case a := <- floorReached:
+			fmt.Printf("%+v\n", a)
+
+		case a := <- obstructionChange:
+			fmt.Printf("%+v\n", a)
+			
+		case a := <- stopClick:
+			fmt.Printf("%+v\n", a)
+		}
+    }   
+	var localState State 
+	var 
+}
