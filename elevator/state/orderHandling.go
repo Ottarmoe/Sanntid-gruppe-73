dd538700a764b481438d0ce300e11375f11e76b9
Anders Skaret Johansen <anders.skaret.johansen@gmail.com>
2026-03-06 17:38:36 +0900

    orderHandlingKjørbar

--- elevator/state/orderHandling.go
+++ elevator/state/orderHandling.go
@@ -7,20 +7,20 @@ import (
 )
 
 func handleButton(vw *ElevWorldView, event ButtonEvent) {
-	me := &vw.Elevs[vw.ID]
+	me := &vw.ElevStates[vw.ID]
 	switch event.Button {
 	case BT_HallUp:
-		if me.HallOrders[event.Floor][Up] == HallNO {
-			me.HallOrders[event.Floor][Up] = HallO
+		if me.OrderState.HallOrders[event.Floor][Up] == HallNO {
+			me.OrderState.HallOrders[event.Floor][Up] = HallO
 		}
 	case BT_HallDown:
-		if me.HallOrders[event.Floor][Down] == HallNO {
-			me.HallOrders[event.Floor][Down] = HallO
+		if me.OrderState.HallOrders[event.Floor][Down] == HallNO {
+			me.OrderState.HallOrders[event.Floor][Down] = HallO
 		}
 	case BT_Cab:
-		me.CabOrders[event.Floor] = CabO
-		for _, elev := range vw.Elevs {
-			elev.CabAgreement[event.Floor] = false
+		me.OrderState.CabOrders[event.Floor] = CabO
+		for elev := 0; elev < NumElevators; elev++ {
+			vw.CabAgreement[elev][event.Floor] = false
 		}
 	}
 }
@@ -36,10 +36,8 @@ func findConsensus(wv ElevWorldView) OrdersWithConsesus {
 		elevExists := false
 		for elev := 0; elev < NumElevators; elev++ {
 			peerHallOrders := &wv.ElevStates[elev].OrderState.HallOrders
-			peerCabOrders := &wv.ElevStates[elev].OrderState.CabOrders
 
-
-			if wv.Elevs[elev].NetError == false {
+			if wv.NetError[elev] == false {
 				elevExists = true
 				if peerHallOrders[floor][Up] == HallO {
 					hallDownExists = true
@@ -47,22 +45,25 @@ func findConsensus(wv ElevWorldView) OrdersWithConsesus {
 				if peerHallOrders[floor][Down] == HallO {
 					hallUpExists = true
 				}
-				if wv.Elevs[elev].CabAgreement[floor] == true {
+				if wv.CabAgreement[elev][floor] == true {
 					cabExists = true
 				}
 			}
 		}
-		if (!elevExists || hallDownExists) && (wv.Elevs[wv.ID].HallOrders[floor][Down] == HallO) {
+		HallOrders := &wv.ElevStates[wv.ID].OrderState.HallOrders
+		CabOrders := &wv.ElevStates[wv.ID].OrderState.CabOrders
+
+		if (!elevExists || hallDownExists) && (HallOrders[floor][Down] == HallO) {
 			ordersWithConsesus.HallOrders[floor][Down] = true
 		} else {
 			ordersWithConsesus.HallOrders[floor][Down] = false
 		}
-		if (!elevExists || hallUpExists) && (wv.Elevs[wv.ID].HallOrders[floor][Up] == HallO) {
+		if (!elevExists || hallUpExists) && (HallOrders[floor][Up] == HallO) {
 			ordersWithConsesus.HallOrders[floor][Up] = true
 		} else {
 			ordersWithConsesus.HallOrders[floor][Up] = false
 		}
-		if (!elevExists || cabExists) && (wv.Elevs[wv.ID].CabOrders[floor] == CabO) {
+		if (!elevExists || cabExists) && (CabOrders[floor] == CabO) {
 			ordersWithConsesus.CabOrders[floor] = true
 		} else {
 			ordersWithConsesus.CabOrders[floor] = false
