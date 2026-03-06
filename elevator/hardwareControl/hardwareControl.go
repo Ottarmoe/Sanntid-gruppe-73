3f45e1b9528584e52ff0faa81ed8d0312a161311
Melina Rahimi <melina@dhcp-10-22-163-194.wlan.ntnu.no>
2026-03-06 09:22:39 +0100

    add hardwareControl.go functionality

--- elevator/hardwareControl/hardwareControl.go
+++ elevator/hardwareControl/hardwareControl.go
@@ -9,10 +9,27 @@ import (
 func HardWareControl(physicsToHardware <-chan PhysicalState, ordersWithConsesusToHardware <-chan OrdersWithConsesus) {
 	for {
 		select {
-		case physicalState := <-physicsToHardware:
+		case physicalState := <-physicsToHardware:	
 			SetFloorIndicator(physicalState.Floor)
 
-			SetMotorDirection
+			if physicalState.Behaviour == Idle {
+				SetMotorDirection(0)
+				// SetDoorOpenLamp(false)
+			}
+
+			if physicalState.Behaviour == Moving{
+				// SetDoorOpenLamp(false)
+				if physicalState.MovDirection == Up {
+					SetMotorDirection(1)
+				}
+				if physicalState.MovDirection == Down {
+					SetMotorDirection(-1)
+				}
+			}
+			if physicalState.Behaviour == DoorOpen {
+				SetMotorDirection(0)
+				SetDoorOpenLamp(true)
+			}
 
 		case ordersWithConsesus := <-ordersWithConsesusToHardware:
 			for floor := 0; floor < NumFloors; floor++ {
