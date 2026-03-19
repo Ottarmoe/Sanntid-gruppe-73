fd1342c4f5bfc0d9221c6d7ecdc15c898eceba62
Anders Skaret Johansen <anders.skaret.johansen@gmail.com>
2026-03-19 22:03:24 +0900

    comment network

--- elevator/network/network.go
+++ elevator/network/network.go
@@ -4,42 +4,51 @@ import (
 	"elevator/networkLow"
 	. "elevator/stateTypes"
 	. "elevator/elevatorConstants"
-	"fmt"
 	"time"
 	"encoding/json"
 	"log"
 )
 
+//Stores a network message, which contains the newest state information. 
+//The routine periodically broadcasts the stored message on the network. 
 func NetworkSender(netMessageToNetworkSender <-chan NetMessage, pokeState chan<- struct{}) {
 	timeToSend := time.NewTicker(time.Duration(BroadcastRate * float64(time.Second)))
 
+	//No periodic sending before the first netmessage has been comunnicated by state
 	netMessage := <-netMessageToNetworkSender
 
 	for {
 		select {
 		case netMessage = <-netMessageToNetworkSender:
+			//Store newest netMessage
 
 		case <-timeToSend.C:
 			pokeState <- struct{}{}
 			data, err := json.Marshal(netMessage)
-			//fmt.Println(string(data))
 			if err != nil {
-				log.Println("send jsonmarshal error:", err)
+				log.Println("Send json marshal error:", err)
+				continue
 			}
 			err = networkLow.Send(data)
 			if err != nil {
-				log.Println("send error:", err)
+				log.Println("Send error:", err)
 			}
 		}
 	}
 }
 
+
+//Receives network messages, filters out duplicate messages and tracks how often
+//a message is received from each elevator. If no message is received within a given interval,
+//the elevator is marked as neterror. To remove neterror, a certain number of messages must be received
+//during an interval, for x intervals in a row. Unique netmessages and neterror-change is sent to state.
 func NetworkReceiver(netMessageToState chan<- NetMessage, netErrorToState chan<- NetErrorNotification) {
 	var prevNetMessages [NumElevators]NetMessage
 
 	receiveMessage := make(chan NetMessage)
 	go receiver(receiveMessage)
 
+	//Track network error state and received messages per interval, nr. of intervals
 	var NetError [NumElevators]bool
 	var ReceivedDuringInterval [NumElevators]int
 	var ReceivedIntervals [NumElevators]int
@@ -48,6 +57,8 @@ func NetworkReceiver(netMessageToState chan<- NetMessage, netErrorToState chan<-
 		ReceivedDuringInterval[i] = 0
 		ReceivedIntervals[i] = 0
 	}
+	
+	// Start a timeout notifier for each other elevator
 	timeout := make(chan int)
 	var resetTimer [NumElevators]chan struct{}
 	for i := 0; i < NumElevators; i++ {
@@ -55,104 +66,90 @@ func NetworkReceiver(netMessageToState chan<- NetMessage, netErrorToState chan<-
 			continue
 		}
 		resetTimer[i] = make(chan struct{}, 1)
-		go timoutNotifier(i, timeout, resetTimer[i])
+		go timeoutNotifier(i, timeout, resetTimer[i])
 	}
 
 	for {
 		select {
 		case netMessage := <-receiveMessage:
-			//Handle neterror
+			//If in neterror: handle getting back online
 			if NetError[netMessage.ID] {
 				ReceivedDuringInterval[netMessage.ID]++
 				if ReceivedDuringInterval[netMessage.ID] < MessagesNeededWithinInterval {
-					continue
+					continue // not enough messages received yet
 				} else {
 					ReceivedIntervals[netMessage.ID]++
 					ReceivedDuringInterval[netMessage.ID] = 0
 					if ReceivedIntervals[netMessage.ID] < IntervalsNeeded {
-						continue
+						continue // not enough intervals
 					} else {
+						//Elevator is now back online
 						NetError[netMessage.ID] = false
 						netErrorToState <- NetErrorNotification{ID: netMessage.ID, NetError: false}
 					}
 				}
 			}
-			resetTimer[netMessage.ID] <- struct{}{} //concider making non-blocking...seems to work for now
+			//If no neterror: process message normally
+			resetTimer[netMessage.ID] <- struct{}{}
 
-			//Avoid botherrueing state with duplicate messages
+			//Avoid bothering state with duplicate messages
 			if netMessage == prevNetMessages[netMessage.ID] {
-				//fmt.Print("c")
 				continue
 			}
-			fmt.Print("unique message")
-			fmt.Println(netMessage)
 
 			netMessageToState <- netMessage
 			prevNetMessages[netMessage.ID] = netMessage
 
 		case id := <-timeout:
+			//Handling a timout for an elevator that is online, involves marking as neterror
 			if !NetError[id] {
 				NetError[id] = true
 				netErrorToState <- NetErrorNotification{ID: id, NetError: true}
-				ReceivedDuringInterval[id] = 0
-				ReceivedIntervals[id] = 0
-				resetTimer[id] <- struct{}{}
-			} else {
-				ReceivedDuringInterval[id] = 0
-				ReceivedIntervals[id] = 0
-				resetTimer[id] <- struct{}{}
 			}
+			//Timout during "get back online phase" means start from scratch. 
+			//Timout while online should also reset the timer.
+			ReceivedDuringInterval[id] = 0
+			ReceivedIntervals[id] = 0
+			resetTimer[id] <- struct{}{}
 		}
 	}
 }
 
-func timoutNotifier(id int, timeout chan int, resetTimer chan struct{}) {
+//Monitors a timer for a specific elevator, and notifies
+//the NetworkReceiver when the timer expires, and for which elevator it expired.
+//The timer can be reset using the resetTimer channel.
+func timeoutNotifier(id int, timeout chan int, resetTimer chan struct{}) {
 	timer := time.NewTimer(time.Duration(NetErrorTimerLength * float64(time.Second)))
 	for {
 		select {
 		case <-timer.C:
 			timeout <- id
 		case <-resetTimer:
-			if !timer.Stop() {
-				// Timer already fired, drain the channel to avoid spurious tick
-				//Check documention if this is still needed! https://pkg.go.dev/time#NewTimer
-				select {
-				case <-timer.C:
-				default:
-				}
-			}
+			timer.Stop()
 			timer.Reset(time.Duration(NetErrorTimerLength * float64(time.Second)))
 		}
 	}
 }
 
+//Reads raw data from the network, decodes it into netMessage structs, 
+//and forwards valid messages to NetworkReceiver.
+//Filters out messeages from this elevator.
 func receiver(receiveMessage chan<- NetMessage) {
-	// last := time.Now() //For debugging
-
 	for {
 		data, err := networkLow.Receive()
 		if err != nil {
-			// log.Println("receive  error:", err)
 			continue
 		}
 		var netMessage NetMessage
 		err = json.Unmarshal(data, &netMessage)
-		//fmt.Println("unmarshaled", netMessage)
 		if err != nil {
-			log.Println("receive json unmarshal error:", err)
+			log.Println("Receive json unmarshal error:", err)
 			continue
 		}
 		if netMessage.ID == ID() {
 			continue
 		}
-		//fmt.Println("recieved", netMessage)
-
-		// For debugging
-		// now := time.Now()
-		// fmt.Println(now.Sub(last))
-		// last = now
 
 		receiveMessage <- netMessage
 	}
-
 }
